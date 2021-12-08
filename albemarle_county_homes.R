ec71264ceb5daac9d770d653841ad7729a7dedbe
Clay Ford <joe.clayton.ford@gmail.com>
2021-12-08 09:30:33 -0500

    2021 update with new links

--- albemarle_county_homes.R
+++ albemarle_county_homes.R
@@ -3,21 +3,22 @@
 
 # getting and munging data for ggplot2 workshop
 
-# Albmarle county
+# Albemarle county
 # Office of Geographic Data Services
-# http://www.albemarle.org/department.asp?department=gds&relpage=3914#Parcels
+# https://www.albemarle.org/government/community-development/gis-mapping/gis-data
 
-# Real Estate Information - Primary Card Level Data - this file includes data
+# Real Estate Information
+# Under PARCELS - Primary Card Level Data - this file includes data
 # such as year built, finished square footage, number of rooms, and condition. 
-# http://www.albemarle.org/gds/gisdata/CAMA/CAMA_CardLevelData_TXT.zip
+# https://gisweb.albemarle.org/gisdata/CAMA/GIS_CardLevelData_new_TXT.zip
 
-library(dplyr)
 
-link <- "http://www.albemarle.org/gds/gisdata/CAMA/CAMA_CardLevelData_TXT.zip"
+link <- "https://gisweb.albemarle.org/gisdata/CAMA/GIS_CardLevelData_new_TXT.zip"
 download.file(link, destfile = basename(link))
 unzip(basename(link), list = TRUE) # list files, but don't extract
-unzip(basename(link), "CAMA_CardLevelData.txt") # extract file to working directory
-card_level <- read.csv("CAMA_CardLevelData.txt", stringsAsFactors = FALSE)
+unzip(basename(link)) # extract file to working directory
+card_level <- read.csv("GIS_CardLevelData_new.txt", stringsAsFactors = FALSE, 
+                       na.strings = "NULL")
 names(card_level)
 
 
@@ -25,19 +26,20 @@ names(card_level)
 # about the parcel itself such as owner information, deed acreage value, and
 # assessed value
 
-# http://www.albemarle.org/gds/gisdata/CAMA/CAMA_ParcelInfo_TXT.zip
+# https://gisweb.albemarle.org/gisdata/CAMA/GIS_View_Redacted_ParcelInfo_TXT.zip
 
-link2 <- "http://www.albemarle.org/gds/gisdata/CAMA/CAMA_ParcelInfo_TXT.zip"
+link2 <- "https://gisweb.albemarle.org/gisdata/CAMA/GIS_View_Redacted_ParcelInfo_TXT.zip"
 download.file(link2, destfile = basename(link2))
 unzip(basename(link2), list = TRUE) # list files, but don't extract
-unzip(basename(link2), "CAMA_ParcelInfo.txt") # extract file to working directory
-parcel_level <- read.csv("CAMA_ParcelInfo.txt", stringsAsFactors = FALSE)
+unzip(basename(link2)) # extract file to working directory
+parcel_level <- read.csv("GIS_View_Redacted_ParcelInfo.txt", stringsAsFactors = FALSE,
+                         na.strings = "NULL")
 # subset for parcels with only 1 card
 parcel_level <- subset(parcel_level, Cards == 1)
 
 
 # merge primary card level data with parcel level data
-datAll <- left_join(card_level, parcel_level,by = c("TMP" = "ParcelID"))
+datAll <- dplyr::left_join(card_level, parcel_level,by = c("TMP" = "ParcelID"))
 # datAll <- merge(card_level, parcel_level,by.x = "TMP",by.y = "ParcelID")
 rm(link, link2)
 
@@ -56,17 +58,16 @@ datAll <- subset(datAll, City %in% c("CHARLOTTESVILLE", "EARLYSVILLE", "CROZET",
 datAll <- subset(datAll, datAll$CardType == "R")
 datAll$CardType <- NULL
 datAll$TMP <- NULL
-datAll <- subset(datAll, YearBuilt > 0)
 datAll$Remodeled <- ifelse(datAll$YearRemodeled > 0, 1, 0)
 datAll <- subset(datAll, TotalRooms > 0 & TotalValue > 0)
 datAll <- subset(datAll, Bedroom < 20)
 datAll <- subset(datAll, TotalRooms < 60)
 datAll <- subset(datAll, TotalValue < 12000000)
 datAll <- subset(datAll, FinSqFt > 0)
-datAll$Condition <- trimws(datAll$Condition)
-datAll <- subset(datAll, Condition != "")
+datAll <- subset(datAll, !is.na(Condition))
 
-vars <- c("YearBuilt", "YearRemodeled", "Condition", "NumStories", "FinSqFt", "Bedroom", "FullBath", 
+vars <- c("YearBuilt", "YearRemodeled", "Condition", "NumStories", 
+          "FinSqFt", "Bedroom", "FullBath", 
           "HalfBath", "TotalRooms", "LotSize", "TotalValue", "City")
 datAll <- datAll[,vars]
 
